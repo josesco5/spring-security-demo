4ed1c73712cf1623754d19a4bd6f7b6b84f02b78
José Escorche <josesco5@gmail.com>
2024-01-08 22:27:19 -0300

    Initial commit

---
+++ src/main/java/com/avilapps/springsecuritydemo/SpringDecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.avilapps.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avilapps/springsecuritydemo/SpringDecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avilapps.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
