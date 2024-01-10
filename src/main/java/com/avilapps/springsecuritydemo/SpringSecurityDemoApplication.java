c0dec98dfa71c42e8483dd13f8e7ddddb76a52c1
José Escorche <josesco5@gmail.com>
2024-01-09 22:00:25 -0300

    Rename application class

--- src/main/java/com/avilapps/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/avilapps/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDecurityDemoApplication {
+public class SpringSecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDecurityDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    }
 
 }
