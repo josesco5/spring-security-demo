77e981a56953612d83e4d130b863fe2dfc2ec854
José Escorche <josesco5@gmail.com>
2024-03-04 21:40:37 -0300

    Configure users through InMemoryUserDetailsManager
    Note: This approach is not recommended for production usage

--- src/main/java/com/avilapps/springsecuritydemo/config/ProjectSecurityConfig.java
+++ src/main/java/com/avilapps/springsecuritydemo/config/ProjectSecurityConfig.java
@@ -2,10 +2,11 @@ package com.avilapps.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +22,22 @@ public class ProjectSecurityConfig {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
