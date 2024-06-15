a678c5747979d4f9d536bee2caa0505cd6b43725
José Escorche <josesco5@gmail.com>
2024-06-15 18:22:51 -0400

    Hash user's password
    * Use BCryptPasswordEncoder to hash passwords

--- src/main/java/com/avilapps/springsecuritydemo/config/ProjectSecurityConfig.java
+++ src/main/java/com/avilapps/springsecuritydemo/config/ProjectSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,6 +30,6 @@ public class ProjectSecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/avilapps/springsecuritydemo/domain/UserService.java
+++ src/main/java/com/avilapps/springsecuritydemo/domain/UserService.java
@@ -2,17 +2,22 @@ package com.avilapps.springsecuritydemo.domain;
 
 import com.avilapps.springsecuritydemo.data.entities.Customer;
 import com.avilapps.springsecuritydemo.data.repository.CustomerRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final PasswordEncoder passwordEncoder;
     private final CustomerRepository customerRepository;
 
-    public UserService(CustomerRepository customerRepository) {
+    public UserService(PasswordEncoder passwordEncoder, CustomerRepository customerRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.customerRepository = customerRepository;
     }
 
     public void registerUser(Customer customer) {
+        String hashedPassword = passwordEncoder.encode(customer.getPwd());
+        customer.setPwd(hashedPassword);
         customerRepository.save(customer);
     }
 }
