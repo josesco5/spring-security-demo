7f7d4fb5a6bb509a2b759edd6a86ed6d82646f1a
José Escorche <josesco5@gmail.com>
2024-09-15 20:42:05 -0300

    Add Spring profile
    * Add profile for production environment
    * Add logger in Notices controller

--- src/main/java/com/avilapps/springsecuritydemo/controller/NoticesController.java
+++ src/main/java/com/avilapps/springsecuritydemo/controller/NoticesController.java
@@ -1,13 +1,22 @@
 package com.avilapps.springsecuritydemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.invoke.MethodHandles;
+
 @RestController
 public class NoticesController {
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @GetMapping("/notices")
     public String getNotices() {
+        LOG.trace("This is a TRACE log");
+        LOG.debug("This is a DEBUG log");
+        LOG.info("This is a INFO log");
+        LOG.error("This is a ERROR log");
         return "Here are the notices details from the BD";
     }
 }
